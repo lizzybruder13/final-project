658a2de800d41291d7cd9e68430939663fed967a
vagrant <vagrant@vagrant.vm>
2020-03-04 22:36:24 +0000

    worked on navbar and css

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 
 
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
--- seed.py
+++ seed.py
@@ -2,5 +2,8 @@
 
 from sqlalchemy import func
 from model import connect_to_db, db, Event, Location, Event_Type
+from datetime import datetime
 from server import app
+
+
 
