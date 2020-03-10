68abb63489dde1496fed855a18d8d085e77c674b
vagrant <vagrant@vagrant.vm>
2020-03-10 21:27:52 +0000

    Added routes to server, created new templates.

--- server.py
+++ server.py
@@ -16,12 +16,49 @@ app.secret_key = "SECRET_KEY"
 
 app.jinja_env.undefined = StrictUndefined
 
+DAYS_OF_THE_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+                    'Friday', 'Saturday']
+
 
 @app.route('/')
 def index():
     """Homepage."""
 
     return render_template('homepage.html')
+
+
+@app.route('/all_events')
+def all_events():
+    """Display a list of all events with information."""
+
+    events = Event.query.all()
+
+    return render_template('all_events.html', events=events)
+
+
+@app.route('/all_locations')
+def all_locations():
+    """Display a list of all locations with information."""
+
+    locations = Location.query.all()
+
+    return render_template('all_locations.html', locations=locations)
+
+
+@app.route('/all_event_types')
+def all_event_types():
+    """Display a list of all event types."""
+
+    event_types = Event_Type.query.all()
+
+    return render_template('all_event_types.html', event_types=event_types)
+
+
+@app.route('/search_forms')
+def search_forms():
+    """Display forms for different types of searches."""
+
+    return render_template('search_forms.html', dow=DAYS_OF_THE_WEEK)
 
 
 @app.route('/search_type')
@@ -35,7 +72,11 @@ def search_type():
 def search_dow():
     """Display event given a certain day of the week."""
 
-    return 'Hey'
+    weekday = request.args.get('weekday')
+
+    events_dow = Event.query.filter_by(weekday=weekday).all()
+
+    print(weekday)
 
 
 if __name__ == "__main__":
